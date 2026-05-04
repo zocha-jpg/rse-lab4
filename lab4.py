82f01a0775f5d6c0d1a5f770a08474ae1b443d99
Zofia Jedlińska <zofiajedlinska@MacBook-Air-Zofia.local>
2026-05-04 08:28:01 +0200

    commit

--- lab4.py
+++ lab4.py
@@ -1,4 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+
 def my_function():
   print("Hello from my function")
 
+def f(x):
+    return x**2 + 10 * np.sin(x)
+
+x = np.arange(-5, 5, 0.1)
+plt.plot(x, f(x));
 my_function()
